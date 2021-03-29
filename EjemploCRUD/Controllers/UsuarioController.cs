83ced7ad9356ad1113ca3b3edfc9456d55e68aef
Hechicero <hechicero5000@hotmail.es>
2021-03-29 07:28:47 -0600

    Add project files.

---
+++ EjemploCRUD/Controllers/UsuarioController.cs
@@ -0,0 +1,125 @@
+﻿using EjemploCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploCRUD.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            List<ListaUsuarioViewModel> lst;
+
+            using(RegistroEntities db = new RegistroEntities())
+            {
+                lst = (from lista in db.Usuario
+                      select new ListaUsuarioViewModel
+                      {
+                          Id = lista.Id,
+                          Nombre = lista.Nombre,
+                          Correo = lista.Correo,
+                          Usuario = lista.Usuario1,
+                          Contrasena = lista.Contrasena
+                      }).ToList();
+            }
+
+            return View(lst);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(UsuarioViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using(RegistroEntities db = new RegistroEntities())
+                    {
+                        var oUsuario = new Usuario();
+                        oUsuario.Nombre = model.Nombre;
+                        oUsuario.Correo = model.Correo;
+                        oUsuario.Usuario1 = model.Usuario;
+                        oUsuario.Contrasena = model.Contrasena;
+
+                        db.Usuario.Add(oUsuario);
+                        db.SaveChanges();
+                    }                    
+                }
+                return Redirect("/Usuario");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            UsuarioViewModel model = new UsuarioViewModel();
+
+            using(RegistroEntities db = new RegistroEntities())
+            {
+                var oUsuario = db.Usuario.Find(id);
+                model.Nombre = oUsuario.Nombre;
+                model.Correo = oUsuario.Correo;
+                model.Usuario = oUsuario.Usuario1;
+                model.Contrasena = oUsuario.Contrasena;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(UsuarioViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using(RegistroEntities db = new RegistroEntities())
+                    {
+                        var oUsuario = db.Usuario.Find(model.Id);
+                        oUsuario.Nombre = model.Nombre;
+                        oUsuario.Correo = model.Correo;
+                        oUsuario.Usuario1 = model.Usuario;
+                        oUsuario.Contrasena = model.Contrasena;
+
+                        db.Entry(oUsuario).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("/Usuario");
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int Id)
+        {
+            using(RegistroEntities db = new RegistroEntities())
+            {
+                var oUsuario = db.Usuario.Find(Id);
+                db.Usuario.Remove(oUsuario);
+                db.SaveChanges();
+            }
+
+            return Redirect("/Usuario");
+        }
+    }
+}
---
+++ EjemploCRUD/Models/UsuarioViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EjemploCRUD.Models
+{
+    public class UsuarioViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        [Display(Name = "Correo Electronico")]
+        public string Correo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Usuario")]
+        public string Usuario { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Constraseña")]
+        public string Contrasena { get; set; }
+    }
+}
